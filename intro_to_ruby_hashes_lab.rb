dfa9c22828ccedc7eb5977adb948a46b19191dfb
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-08 01:34:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
     new_Hash = {:railroads => {
     :pieces => 4, 
     :rent_in_dollars => {
-      :key_1 => "value", :key_2 => "value", :key_3 => "value", :key_4 => "value"}}
+      :name => "value", :key_2 => "value", :key_3 => "value", :key_4 => "value"}}
   }
 end
 
